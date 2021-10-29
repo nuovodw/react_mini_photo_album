409992e8bf15183f3b1689c6909c038fd1946e07
Daniel Won <dan.j.won@gmail.com>
2021-10-29 06:03:24 -0500

    add menu bar and heading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import {
+	Typography,
+	AppBar,
+	Card,
+	CardActions,
+	CardContent,
+	CardMedia,
+	CssBaseline,
+	Grid,
+	Toolbar,
+	Container,
+} from '@material-ui/core';
+import { PhotoCamera } from '@material-ui/icons';
 
 const App = () => {
 	return (
-		<div>
-			<h1>Mini Photo Album</h1>
-		</div>
+		<>
+			<CssBaseline />
+			<AppBar position="relative">
+				<Toolbar>
+					<PhotoCamera />
+					<Typography variant="h6">Mini Photo Album</Typography>
+				</Toolbar>
+			</AppBar>
+
+			<main>
+				<div>
+					<container maxWidth="sm">
+						<Typography
+							variant="h2"
+							align="center"
+							color="textPrimary"
+							gutterBottom
+						>
+							Mini Photo Album
+						</Typography>
+					</container>
+				</div>
+			</main>
+		</>
 	);
 };
 

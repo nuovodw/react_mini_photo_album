1c7452407f10eb55195c74613ca5166734d64c0d
Daniel Won <dan.j.won@gmail.com>
2021-10-29 06:35:07 -0500

    add paraph and buttons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
 	Typography,
 	AppBar,
+	Button,
 	Card,
 	CardActions,
 	CardContent,
@@ -26,7 +27,7 @@ const App = () => {
 
 			<main>
 				<div>
-					<container maxWidth="sm">
+					<Container maxWidth="sm" style={{ marginTop: '100px' }}>
 						<Typography
 							variant="h2"
 							align="center"
@@ -35,7 +36,32 @@ const App = () => {
 						>
 							Mini Photo Album
 						</Typography>
-					</container>
+						<Typography
+							variant="h6"
+							align="center"
+							color="textSecondary"
+							paragraph
+						>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+							cupiditate amet et ipsum ratione voluptates in? Quia, similique
+							incidunt. Culpa harum ipsa recusandae temporibus natus voluptas
+							ratione eaque, adipisci cupiditate?
+						</Typography>
+						<div>
+							<Grid container spacing={2} justify="center">
+								<Grid item>
+									<Button variant="contained" color="primary">
+										See photos
+									</Button>
+								</Grid>
+								<Grid item>
+									<Button variant="outlined" color="primary">
+										Contact me
+									</Button>
+								</Grid>
+							</Grid>
+						</div>
+					</Container>
 				</div>
 			</main>
 		</>
